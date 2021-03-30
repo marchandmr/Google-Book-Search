a2e3e908d04aa16da5854d61117ed0b011cae5ea
Matthew Marchand <marchandmr@icloud.com>
2021-03-29 20:23:09 -0500

    trying to upload to atlas

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-book-list",
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booklist",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
